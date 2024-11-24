f747063f5f10c20e924753074e55b01341ef1862
kevintsao14 <chtsao@syr.edu>
2024-11-23 22:54:41 -0800

    edit module

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -12,4 +12,11 @@ export function createModule(module) {
     const { modules } = Database;
     Database.modules = modules.filter((module) => module._id !== moduleId);
    }
-   
+
+   export function updateModule(moduleId, moduleUpdates) {
+    const { modules } = Database;
+    const module = modules.find((module) => module._id === moduleId);
+    Object.assign(module, moduleUpdates);
+    return module;
+  }
+  
