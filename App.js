82a6cd382f2c77445f1e614d5980ad7b8ef4ab9c
kevintsao14 <chtsao@syr.edu>
2024-12-03 23:44:58 -0800

    a6 mongodb

--- App.js
+++ App.js
@@ -7,8 +7,13 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import UserRoutes from "./Kanbas/Users/routes.js";
-// import EnrollmentRoutes from "./Kanbas/Enrollments/route.js";
+import EnrollmentRoutes from "./Kanbas/Enrollments/route.js";
 import cors from "cors";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(
     cors({
