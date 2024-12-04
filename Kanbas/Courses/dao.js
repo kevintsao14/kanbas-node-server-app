1b3de214236468a6b5ca19f937240889c1acaba5
kevintsao14 <chtsao@syr.edu>
2024-12-04 02:10:24 -0800

    update courses

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -5,20 +5,20 @@ export function findAllCourses() {
   return model.find();
 }
 export function findCoursesForEnrolledUser(userId) {
-    const { courses, enrollments } = Database;
-    const enrolledCourses = courses.filter((course) =>
-      enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
-    return enrolledCourses;
-  }
+  const { courses, enrollments } = Database;
+  const enrolledCourses = courses.filter((course) =>
+    enrollments.some((enrollment) => enrollment.user === userId && enrollment.course === course._id));
+  return enrolledCourses;
+}
 
-  export function createCourse(course) {
-    delete course._id;
-    return model.create(course);
-    // const newCourse = { ...course, _id: Date.now().toString() };
-    // Database.courses = [...Database.courses, newCourse];
-    // return newCourse;
-  }
-  
+export function createCourse(course) {
+  delete course._id;
+  return model.create(course);
+  // const newCourse = { ...course, _id: Date.now().toString() };
+  // Database.courses = [...Database.courses, newCourse];
+  // return newCourse;
+}
+
 export function deleteCourse(courseId) {
   return model.deleteOne({ _id: courseId });
   // const { courses, enrollments } = Database;
@@ -28,9 +28,9 @@ export function deleteCourse(courseId) {
 }
 
 export function updateCourse(courseId, courseUpdates) {
-    const { courses } = Database;
-    const course = courses.find((course) => course._id === courseId);
-    Object.assign(course, courseUpdates);
-    return course;
-  }
-  
+  return model.updateOne({ _id: courseId }, { $set: courseUpdates });
+  // const { courses } = Database;
+  // const course = courses.find((course) => course._id === courseId);
+  // Object.assign(course, courseUpdates);
+  // return course;
+}
